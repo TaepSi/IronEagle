3d44e7d8d3dbc4767b8cc574189b5a2d0dac0ff3
Vladimir <86762075+TaepSi@users.noreply.github.com>
2026-05-11 18:49:35 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
 import discord
 from discord.ext import commands
 import os
+from flask import Flask
+from threading import Thread
 
+# --- БЛОК ОЖИВЛЯЛКИ ---
+app = Flask('')
+@app.route('/')
+def home(): return "Iron Eagle is online!"
+
+def run_web():
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
+
+def keep_alive():
+    t = Thread(target=run_web)
+    t.daemon = True
+    t.start()
+
+# --- НАСТРОЙКИ ---
 TOKEN = os.getenv("DISCORD_TOKEN")
 
 intents = discord.Intents.all()
@@ -16,4 +33,5 @@ async def ping(ctx):
     await ctx.send("🦅 Клёкот!")
 
 if __name__ == "__main__":
+    keep_alive()
     bot.run(TOKEN)
